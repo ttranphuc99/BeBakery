3bdf1ec4b211f432727632180ed85ed4f8823d4c
Thien Phuc <ttran.phuc99@gmail.com>
2018-04-14 20:48:19 +0700

    Edit name of files & download picture to local host

---
+++ js/info1.js
@@ -0,0 +1,7 @@
+//Animation appear on info 1
+function myFunction() {
+    document.getElementById('animation1').style.opacity = "1";
+    document.getElementById('animation2').style.opacity = "1";
+            
+    document.getElementById('animation3').style.opacity = "1";
+}
