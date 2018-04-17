60a4091994f94306116207930d3c37a80e006627
Thien Phuc <ttran.phuc99@gmail.com>
2018-04-17 11:14:41 +0700

    clear branch

--- js/info1.js
+++
@@ -1,7 +0,0 @@
-//Animation appear on info 1
-function myFunction() {
-    document.getElementById('animation1').style.opacity = "1";
-    document.getElementById('animation2').style.opacity = "1";
-            
-    document.getElementById('animation3').style.opacity = "1";
-}
